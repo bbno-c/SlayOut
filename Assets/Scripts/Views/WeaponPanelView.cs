ccae4f53e8288c299e0c929d9904f062a6f8efd8
bbno-c <khorolskiy.o.s@gmail.com>
2020-07-14 20:56:56 +0300

    0

--- Assets/Scripts/Views/WeaponPanelView.cs
+++ Assets/Scripts/Views/WeaponPanelView.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class WeaponPanelView : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject WeaponPanel;
     // Start is called before the first frame update
     void Start()
     {
